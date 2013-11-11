3ffdd38ef698978d24c379dd2e9b98621d1445bf
Jason Racine <jason.racine@he-arc.ch>
2013-11-11 13:07:01 +0100

    Added configuration and file for defining our self layout in module/ArcAnswer/view/layout/layout.phtml

--- module/ArcAnswer/config/module.config.php
+++ module/ArcAnswer/config/module.config.php
@@ -140,6 +140,9 @@ return array(
 	),
 
 	'view_manager' => array(
+		'template_map' => array(
+			'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+		),
 		'template_path_stack' => array(
 			'arcanswer' => __DIR__ . '/../view',
 		),
