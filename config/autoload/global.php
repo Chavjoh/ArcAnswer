1592813dd7464a4ce93263ac96b547be20f0da41
David Kuhner <dav.kuhn@gmail.com>
2013-11-20 14:00:50 +0100

    Moved password, username, dbname into local.php, who is not committed into version control. Copy local.php.dist to local.php and fill the gaps. Global.php must not include passwords or other sensitive information.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,18 +12,4 @@
  */
 
 return array(
-	'doctrine' => array(
-		'connection' => array(
-			'orm_default' => array(
-				'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
-				'params' => array(
-					'host' => 'localhost',
-					'port' => '3306',
-					'user' => 'arcanswer',
-					'password' => 'arcanswer',
-					'dbname' => 'arcanswer',
-				),
-			),
-		),
-	),
 );
